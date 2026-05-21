d31989963088f8d4ee7e55198d5699b12c830408
Your Name <youremail@example.com>
2026-05-21 15:49:42 +0530

    interceptor add in api config

--- frontend/src/components/services/apiConfig.js
+++ frontend/src/components/services/apiConfig.js
@@ -4,7 +4,20 @@ const BASE_URL = 'https://ecommerce-2hvv.onrender.com/api';
 
 const api = axios.create({
     baseURL: BASE_URL,
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+    }
 })
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
 
 export default api
