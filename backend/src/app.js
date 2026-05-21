f151f7404d287bbab957b7a85b4d52b219ab3c96
Your Name <youremail@example.com>
2026-05-21 15:35:57 +0530

    something

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,6 +26,8 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
+app.options('*', cors(corsOptions));
+
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(cookieParser())
