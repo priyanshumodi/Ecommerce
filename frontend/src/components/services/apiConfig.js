330d8dc47d44d05cce352f3fb34ad2d3c5b0f199
Your Name <youremail@example.com>
2026-05-21 15:05:30 +0530

    api change

--- frontend/src/components/services/apiConfig.js
+++ frontend/src/components/services/apiConfig.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL = 'http://localhost:8000/api';
+// const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = 'https://ecommerce-2hvv.onrender.com/api';
 
 const api = axios.create({
     baseURL: BASE_URL,
