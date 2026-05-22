ca2d0bd9e42f6fb6dcd39e4fc89e6de3825c59ff
Your Name <youremail@example.com>
2026-05-22 12:55:45 +0530

    update initial state using cookie

--- frontend/src/components/features/userSlice.js
+++ frontend/src/components/features/userSlice.js
@@ -9,6 +9,11 @@ import Cookies from 'js-cookie';
 
 const accessToken = Cookies.get('accessToken');
 
+if (!accessToken) {
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('user')
+}
+
 export const loginUser = createAsyncThunk(
     'user/loginUser',
     async (formData, { rejectWithValue }) => {
@@ -23,7 +28,7 @@ export const loginUser = createAsyncThunk(
 
 export const logoutUser = createAsyncThunk(
     'user/logoutUser',
-    async (_, {rejectWithValue}) => {
+    async (_, { rejectWithValue }) => {
         try {
             const user = await logoutUserApi();
             return user
@@ -81,8 +86,8 @@ const initialState = {
             email: "priyanshumodi944@gmail.com"
         },
     ],
-    currentUser: accessToken ? (JSON.parse(localStorage.getItem('user')) || null) : null,
-    isAuthenticated: accessToken ? (!!localStorage.getItem('accessToken')) : false,
+    currentUser: (JSON.parse(localStorage.getItem('user')) || null),
+    isAuthenticated: (!!localStorage.getItem('accessToken')),
     error: null,
     isLoading: false
 }
@@ -91,93 +96,93 @@ export const userSlice = createSlice({
     name: 'user',
     initialState,
     extraReducers: (builder) => {
-            builder
-                .addCase(loginUser.pending, (state) => {
-                    state.isLoading = true;
-                })
-                .addCase(loginUser.fulfilled, (state, action) => {
-                    const response = action.payload;
-                    const user = {
-                        _id: response?.user?._id, 
-                        firstName: response?.user?.firstName, 
-                        lastName: response?.user?.lastName,
-                        email: response?.user?.email,
-                        role: response?.user?.role
-                    }
+        builder
+            .addCase(loginUser.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(loginUser.fulfilled, (state, action) => {
+                const response = action.payload;
+                const user = {
+                    _id: response?.user?._id,
+                    firstName: response?.user?.firstName,
+                    lastName: response?.user?.lastName,
+                    email: response?.user?.email,
+                    role: response?.user?.role
+                }
 
-                    state.isLoading = false;
-                    state.currentUser = user;
-                    state.isAuthenticated = true;
-                    state.error = null;
+                state.isLoading = false;
+                state.currentUser = user;
+                state.isAuthenticated = true;
+                state.error = null;
 
 
-                    localStorage.setItem('accessToken', response?.accessToken)
-                    localStorage.setItem('user', JSON.stringify(user))
+                localStorage.setItem('accessToken', response?.accessToken)
+                localStorage.setItem('user', JSON.stringify(user))
 
-                    toast.success(`Welcome ${user.firstName}`)
-                })
-                .addCase(loginUser.rejected, (state, action) => {
-                    state.isLoading = false;
-                    state.error = action.payload;
-                })
-                .addCase(logoutUser.pending, (state) => {
-                    state.isLoading = true;
-                })
-                .addCase(logoutUser.fulfilled, (state) => {
-                    state.isLoading = false;
-                    state.currentUser = null;
-                    state.isAuthenticated = false;
-                    state.error = null;
-                    state.users = [];
+                toast.success(`Welcome ${user.firstName}`)
+            })
+            .addCase(loginUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            })
+            .addCase(logoutUser.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(logoutUser.fulfilled, (state) => {
+                state.isLoading = false;
+                state.currentUser = null;
+                state.isAuthenticated = false;
+                state.error = null;
+                state.users = [];
 
-                    localStorage.removeItem('accessToken')
-                    localStorage.removeItem('user')
-                })
-                .addCase(logoutUser.rejected, (state, action) => {
-                    state.isLoading = false;
-                    state.error = action.payload;
-                })
-                .addCase(fetchAllUsers.pending, (state) => {
-                    state.isLoading = true;
-                })
-                .addCase(fetchAllUsers.fulfilled, (state, action) => {
-                    state.users = action.payload;
-                    state.isLoading = false;
-                    state.error = null;
-                })
-                .addCase(fetchAllUsers.rejected, (state, action) => {
-                    state.isLoading = false;
-                    state.error = action.payload;
-                })
-                .addCase(fetchUsersChats.pending, (state) => {
-                    state.isLoading = true;
-                })
-                .addCase(fetchUsersChats.fulfilled, (state, action) => {
-                    state.users = action.payload;
-                    state.isLoading = false;
-                    state.error = null;
-                })
-                .addCase(fetchUsersChats.rejected, (state, action) => {
-                    state.isLoading = false;
-                    state.error = action.payload;
-                })
-                .addCase(registerUser.pending, (state) => {
-                    state.isLoading = true;
-                })
-                .addCase(registerUser.fulfilled, (state, action) => {
-                    state.isLoading = false;
-                    state.error = null;
-                    state.isAuthenticated = false;
-                    state.users = [];
-                    state.currentUser = null;
-                    
-                    toast.success(`registeration successfull....`)
-                })
-                .addCase(registerUser.rejected, (state, action) => {
-                    state.isLoading = false;
-                    state.error = action.payload;
-                })
-        }
+                localStorage.removeItem('accessToken')
+                localStorage.removeItem('user')
+            })
+            .addCase(logoutUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            })
+            .addCase(fetchAllUsers.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(fetchAllUsers.fulfilled, (state, action) => {
+                state.users = action.payload;
+                state.isLoading = false;
+                state.error = null;
+            })
+            .addCase(fetchAllUsers.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            })
+            .addCase(fetchUsersChats.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(fetchUsersChats.fulfilled, (state, action) => {
+                state.users = action.payload;
+                state.isLoading = false;
+                state.error = null;
+            })
+            .addCase(fetchUsersChats.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            })
+            .addCase(registerUser.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(registerUser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                state.isAuthenticated = false;
+                state.users = [];
+                state.currentUser = null;
+
+                toast.success(`registeration successfull....`)
+            })
+            .addCase(registerUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            })
+    }
 })
 
 export default userSlice.reducer
