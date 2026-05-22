1a916da2c4b0847f14a8fe6e0f838c7463ce4957
Your Name <youremail@example.com>
2026-05-22 14:30:39 +0530

    debug app

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,7 +27,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
-app.options('*', cors(corsOptions));
+app.options('*splat', cors(corsOptions));
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
